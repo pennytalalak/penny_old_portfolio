578e6c5359dc05dfafe162614d97a83614855640
pennytalalak <36777961+pennytalalak@users.noreply.github.com>
2018-07-29 19:21:33 +1000

    stadium

---
+++ stadium/js/extra.js
@@ -0,0 +1,20 @@
+$(".render").click(function(){
+  $(".karamba").fadeIn(1000);
+  $(".render").fadeOut(1000);
+});
+
+$(".karamba").click(function(){
+  $(".karamba").fadeOut(1000);
+  $(".render").fadeIn(1000);
+});
+
+//Karam
+$(".karam1").click(function(){
+  $(".karam2").fadeIn(1000);
+  $(".karam1").fadeOut(1000);
+});
+
+$(".karam2").click(function(){
+  $(".karam2").fadeOut(1000);
+  $(".karam1").fadeIn(1000);
+});
