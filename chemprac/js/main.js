9b2b697c695625a49a61e852035ec64e87562aa0
pennytalalak <36777961+pennytalalak@users.noreply.github.com>
2018-10-26 13:36:25 +1100

    chemprac

---
+++ chemprac/js/main.js
@@ -0,0 +1,55 @@
+var $draggable = $('#beaker').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#conicalflask').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#bunsen').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#burette').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#condenser').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#ebalance').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#funnel').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#hotplate').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#pipette').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#retort').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#roundflask').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#tripod').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#volumetricflask').draggabilly({
+  grid: [ 20, 20 ]
+});
+
+var $draggable = $('#waterbath').draggabilly({
+  grid: [ 20, 20 ]
+});
